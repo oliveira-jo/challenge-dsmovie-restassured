36bbc9e507be835239ec9bda83ce1de816770064
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-10-17 11:43:11 -0300

    Create challenge dsmovie with restassured

---
+++ README.md
@@ -0,0 +1,24 @@
+# Desafio DSMovie RestAssured
+Você deve implementar todos os testes de API conforme solicitado (assista o vídeo explicativo)
+
+## Sobre o projeto DSMovie:
+Este é um projeto de filmes e avaliações de filmes. A visualização dos dados dos filmes é pública (não necessita login), porém as alterações de filmes (inserir, atualizar, deletar) são permitidas apenas para usuários ADMIN. As avaliações de filmes podem ser registradas por qualquer usuário logado CLIENT ou ADMIN. A entidade Score armazena uma nota de 0 a 5 (score) que cada usuário deu a cada filme. Sempre que um usuário registra uma nota, o sistema calcula a média das notas de todos usuários, e armazena essa nota média (score) na entidade Movie, juntamente com a contagem de votos (count).  Veja vídeo explicativo.
+
+![class-diagram](assets/class-diagram.png)
+
+## Abaixo estão os testes de API que você deverá implementar utilizando o RestAssured. O mínimo para aprovação no desafio são 8 dos 10 testes.
+
+### MovieContollerRA:
+ - findAllShouldReturnOkWhenMovieNoArgumentsGiven
+ - findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty
+ - findByIdShouldReturnMovieWhenIdExists
+ - findByIdShouldReturnNotFoundWhenIdDoesNotExist
+ - insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle
+ - insertShouldReturnForbiddenWhenClientLogged
+ - insertShouldReturnUnauthorizedWhenInvalidToken
+
+### ScoreContollerRA:
+ - saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist
+ - saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId
+ - saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero
+
---
+++ src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -0,0 +1,98 @@
+package com.devsuperior.dsmovie.controllers;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.devsuperior.dsmovie.tests.TokenUtil;
+
+import io.restassured.http.ContentType;
+
+public class ScoreControllerRA {
+
+	private String clientUsername, clientPassword, clientToken;
+	private Map<String, Object> postScoreInstance;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+
+		baseURI = "http://localhost:8080";
+		clientUsername = "alex@gmail.com";
+		clientPassword = "123456";
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+		postScoreInstance = new HashMap<>();
+		postScoreInstance.put("movieId", 1);
+		postScoreInstance.put("score", 4);
+
+	}
+
+	@Test
+	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {
+
+		postScoreInstance.put("movieId", 31);
+		JSONObject newScore = new JSONObject(postScoreInstance);
+
+		given()
+				.header("Content-type", "application/json")
+				.header("Authorization", "Bearer " + clientToken)
+				.body(newScore)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.when()
+				.put("/scores")
+				.then()
+				.statusCode(404)
+				.body("error", equalTo("Recurso não encontrado"));
+
+	}
+
+	@Test
+	public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+
+		postScoreInstance = new HashMap<>();
+		postScoreInstance.put("score", 4);
+		JSONObject newScore = new JSONObject(postScoreInstance);
+
+		given()
+				.header("Content-type", "application/json")
+				.header("Authorization", "Bearer " + clientToken)
+				.body(newScore)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.when()
+				.put("/scores")
+				.then()
+				.statusCode(422)
+				.body("errors[0].fieldName", equalTo("movieId"))
+				.body("errors[0].message", equalTo("Campo requerido"));
+
+	}
+
+	@Test
+	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {
+
+		postScoreInstance.put("score", -4);
+		JSONObject newScore = new JSONObject(postScoreInstance);
+
+		given()
+				.header("Content-type", "application/json")
+				.header("Authorization", "Bearer " + clientToken)
+				.body(newScore)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.when()
+				.put("/scores")
+				.then()
+				.statusCode(422)
+				.body("errors[0].fieldName", equalTo("score"))
+				.body("errors[0].message", equalTo("Valor mínimo 0"));
+
+	}
+}
